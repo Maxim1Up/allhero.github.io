fb934e41659c81cf2865ef951a265501401b84d2
MaxUpIly <89905819+MaxUpIly@users.noreply.github.com>
2022-12-02 21:54:49 +0300

    Версия 1.0 проекта

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () =>{
+    if(nav.classList.toggle('open')){
+        navBtnImg.src = "./img/icons/nav-close.svg";
+    }
+    else{
+        navBtnImg.src = "./img/icons/nav-open.svg";
+    }
+}
+
+
+const countDate = new Date(2022, 11, 5, 18, 0, 0, 0).getTime();
+function newYear(){
+    const now = new Date().getTime();
+    gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap/(day));
+    const h = Math.floor((gap %(day) / hour));
+    const m = Math.floor((gap %(hour) / minute));
+    const s = Math.floor((gap %(minute) / second));
+
+    document.getElementById("day").innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+    
+}
+setInterval(function(){
+    newYear();
+},1000)
+AOS.init({
+    once: true
+});
